bdfd5d27e2f06c0d7ff869f88bbb596102801835
David Remiro <david.remiro89@gmail.com>
2024-05-17 20:28:58 +0200

    forms building create action and validation messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,4 +4,28 @@ def show
  @article = Article.find(params[:id])
 end
 
+def index
+@articles = Article.all
+end
+
+def new
+@article=Article.new
+end
+
+def create
+    @article = Article.new(article_params)
+    if @article.save
+        flash[:notice] = "Article was created successfully."
+      redirect_to @article
+    else
+      render 'new', status: 422
+    end
+end
+  
+private
+
+def article_params
+  params.require(:article).permit(:id, :title, :description)
+end
+
 end
