8639cdce1239a6529a914e1d395181891f818442
David Remiro <david.remiro89@gmail.com>
2024-05-22 20:31:47 +0200

    Add user index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,14 @@
 class UsersController < ApplicationController
 
+    def show
+      @user= User.find(params[:id])
+      @articles = @user.articles
+    end
+
+    def index
+      @users = User.all
+    end
+
     def new
       @user = User.new
     end
