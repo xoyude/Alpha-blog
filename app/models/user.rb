d8cbf39c38d38504364064439c3177c205c8ce5e
David Remiro <david.remiro89@gmail.com>
2024-05-23 17:48:52 +0200

    Delete user funcionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
 class User < ApplicationRecord
     before_save { self.email = email.downcase }
-  
-    has_many :articles
-  
+    has_many :articles, dependent: :destroy
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   
     validates :username, presence: true, 
